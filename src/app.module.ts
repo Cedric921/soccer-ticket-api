0c9c5fcd0a960bc77c3bbcdf783d6bb8a14f8f61
cedric karungu <ckarungu921@gmail.com>
2023-05-23 16:06:54 +0200

    feat: gen modules based to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { TeamModule } from './team/team.module';
+import { GameModule } from './game/game.module';
+import { CompetitionModule } from './competition/competition.module';
+import { ReservationModule } from './reservation/reservation.module';
 
 @Module({
   imports: [
@@ -12,6 +16,10 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TeamModule,
+    GameModule,
+    CompetitionModule,
+    ReservationModule,
   ],
   controllers: [],
   providers: [],
