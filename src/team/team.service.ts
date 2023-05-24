3dec0d07b68e0a067db15a2b98fef183f31f0ddd
cedric karungu <ckarungu921@gmail.com>
2023-05-24 11:26:30 +0200

    feat: connect team services to db

--- src/team/team.service.ts
+++ src/team/team.service.ts
@@ -1,23 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { Game, Team } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class TeamService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async getTeams() {
-    return { message: 'teams fetched' };
+  async getTeams(): Promise<{
+    message: string;
+    data: Team[];
+  }> {
+    const data = await this.prisma.team.findMany();
+    return { message: 'teams fetched', data };
   }
 
-  async getTeam(id: string) {
-    return { message: 'team fetched', id };
+  async getTeam(id: string): Promise<{
+    message: string;
+    data: Team & {
+      gamesOne: Game[];
+      gamesTwo: Game[];
+    };
+  }> {
+    const data = await this.prisma.team.findUnique({
+      where: { id },
+      include: { gamesOne: true, gamesTwo: true },
+    });
+    return { message: 'team fetched', data };
   }
 
-  async createTeam(dto: any) {
-    return { message: 'team created', data: dto };
+  async createTeam(dto: any): Promise<{
+    message: string;
+    data: Team;
+  }> {
+    const data = await this.prisma.team.create({ data: dto });
+    return { message: 'team created', data };
   }
 
-  async updateTeam(id: string, dto: any) {
-    return { message: 'team updated', id, dto };
+  async updateTeam(
+    id: string,
+    dto: any,
+  ): Promise<{
+    message: string;
+    data: Team;
+  }> {
+    const data = await this.prisma.team.update({ where: { id }, data: dto });
+    return { message: 'team updated', data };
   }
 }
