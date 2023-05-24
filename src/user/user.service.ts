c0b8dcf10910dd644b4ba22abe79ce66fd852649
cedric karungu <ckarungu921@gmail.com>
2023-05-24 10:46:55 +0200

    feat: add user types

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,12 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from './../prisma/prisma.service';
 import { CreateUserDTO, UpdateUserDTO } from './user.dto';
 import * as argon from 'argon2';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async getUsers() {
+  async getUsers(): Promise<{
+    message: string;
+    data: {
+      id: string;
+      email: string;
+      names: string;
+      role: string;
+      createdAt: Date;
+      updatedAt: Date;
+    }[];
+  }> {
     const users = await this.prisma.user.findMany();
     const data = users.map((user) => {
       const { password, ...others } = user;
@@ -24,7 +35,10 @@ export class UserService {
     return { message: 'get user with success', data: user };
   }
 
-  async create(dto: CreateUserDTO) {
+  async create(dto: CreateUserDTO): Promise<{
+    message: string;
+    data: User;
+  }> {
     const hash = await argon.hash(dto.password);
 
     const user = await this.prisma.user.create({
@@ -35,7 +49,13 @@ export class UserService {
     return { message: 'create user with success', data: user };
   }
 
-  async updateUser(id: string, dto: UpdateUserDTO) {
+  async updateUser(
+    id: string,
+    dto: UpdateUserDTO,
+  ): Promise<{
+    message: string;
+    data: User;
+  }> {
     const user = await this.prisma.user.update({
       where: { id },
       data: { ...dto },
